9dc9174e7638b402366b4270b47c1e323604fe17
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2022-09-12 00:34:45 -0300

    update: remote repository

--- src/main/java/br/com/phricardo/idJava/dto/UserRegisterRequestDto.java
+++ src/main/java/br/com/phricardo/idJava/dto/UserRegisterRequestDto.java
@@ -3,21 +3,11 @@ package br.com.phricardo.idJava.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
 @Getter @Setter
-public class UserRegisterRequestDto {
-    @NotNull @NotEmpty @Pattern(regexp = "^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$", message = "invalid username")
+public class UserRegisterRequestDto  {
     private String username;
-    @NotNull @NotEmpty @Pattern(regexp = "^(?:[\\p{L}\\p{Mn}\\p{Pd}\\'\\x{2019}]+(?:$|\\s+)){2,}$", message = "invalid full name")
     private String fullName;
-    @NotNull @NotEmpty @Pattern(regexp = "^(\\d{2}\\.?\\d{3}\\.?\\d{3}\\/?\\d{4}-?\\d{2}|\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2})$", message = "invalid CPF or CNPJ")
     private String cpfCnpj;
-    @NotNull @NotEmpty @Email(message = "invalid email")
     private String email;
-    @NotNull @NotEmpty @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,20}$", message = "invalid password")
     private String password;
 }
--- src/main/java/br/com/phricardo/idJava/service/RegisterService.java
+++ src/main/java/br/com/phricardo/idJava/service/RegisterService.java
@@ -1,8 +1,10 @@
 package br.com.phricardo.idJava.service;
 
+import br.com.phricardo.idJava.dto.ErrorResponseDto;
 import br.com.phricardo.idJava.dto.UserRegisterRequestDto;
 import br.com.phricardo.idJava.model.UserModel;
 import br.com.phricardo.idJava.repository.UserRepository;
+import br.com.phricardo.idJava.util.PatternMatcherUtil;
 import lombok.AllArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
@@ -19,21 +21,41 @@ import java.util.UUID;
 public class RegisterService {
     private final PasswordEncoder encoder;
     private final UserRepository userRepository;
+    private final PatternMatcherUtil patternMatcherUtil;
 
     public ResponseEntity<?> register(@Valid @RequestBody @NotNull UserRegisterRequestDto userRegisterRequestDto) {
-       UserModel userModel = userRepository.save(
-                UserModel.builder()
-                        .userId(UUID.randomUUID())
-                        .username(userRegisterRequestDto.getUsername())
-                        .fullName(userRegisterRequestDto.getFullName())
-                        .cpfCnpj(userRegisterRequestDto.getCpfCnpj())
-                        .email(userRegisterRequestDto.getEmail())
-                        .password(encoder.encode(userRegisterRequestDto.getPassword()))
-                        .active(true)
-                        .build()
-        );
+            if(!isValidUserRequest(userRegisterRequestDto)) {
+                return new ResponseEntity<>(new ErrorResponseDto("Some field does not respect the required pattern"), HttpStatus.BAD_REQUEST);
+            }
 
-        return new ResponseEntity<>(userModel, HttpStatus.CREATED);
+            UserModel userModel = userRepository.save(
+                    UserModel.builder()
+                            .userId(UUID.randomUUID())
+                            .username(userRegisterRequestDto.getUsername())
+                            .fullName(userRegisterRequestDto.getFullName())
+                            .cpfCnpj(userRegisterRequestDto.getCpfCnpj())
+                            .email(userRegisterRequestDto.getEmail())
+                            .password(encoder.encode(userRegisterRequestDto.getPassword()))
+                            .active(true)
+                            .build()
+            );
+
+            return new ResponseEntity<>(userModel, HttpStatus.CREATED);
+    }
+
+    public Boolean isValidUserRequest(UserRegisterRequestDto userRegisterRequestDto){
+        Boolean username = patternMatcherUtil.matcher("^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$",
+                userRegisterRequestDto.getUsername());
+        Boolean fullName = patternMatcherUtil.matcher("^(?:[\\p{L}\\p{Mn}\\p{Pd}\\'\\x{2019}]+(?:$|\\s+)){2,}$",
+                userRegisterRequestDto.getFullName());
+        Boolean cpfCnpj = patternMatcherUtil.matcher("^(\\d{2}\\.?\\d{3}\\.?\\d{3}\\/?\\d{4}-?\\d{2}|\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2})$",
+                userRegisterRequestDto.getCpfCnpj());
+        Boolean email = patternMatcherUtil.matcher("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$",
+                userRegisterRequestDto.getEmail());
+        Boolean password = patternMatcherUtil.matcher("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,20}$",
+                userRegisterRequestDto.getPassword());
+        Boolean isValid = username && fullName && cpfCnpj && email && password;
+                return isValid;
     }
 
 }
