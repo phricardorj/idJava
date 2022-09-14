5914b075bfff657ce8eb987a89a919cc141f300d
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2022-09-13 21:30:22 -0300

    change(UserRegisterRequestDto): password regex validation

--- src/main/java/br/com/phricardo/idJava/dto/UserRegisterRequestDto.java
+++ src/main/java/br/com/phricardo/idJava/dto/UserRegisterRequestDto.java
@@ -8,14 +8,16 @@ import lombok.Setter;
 
 @Getter @Setter
 public class UserRegisterRequestDto  {
-    @NotEmpty @Pattern(message = "Password does not respect pattern", regexp = "^[\\w](?!.*?\\.{2})[\\w.]{1,28}[\\w]$")
+    public static final String MESSAGE_PATTERN_ERROR = "not respect pattern";
+
+    @NotEmpty @Pattern(message = MESSAGE_PATTERN_ERROR, regexp = "^[\\w](?!.*?\\.{2})[\\w.]{1,28}[\\w]$")
     private String username;
-    @NotBlank @Pattern(message = "fullName \ndoes not respect pattern", regexp = "^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$")
+    @NotBlank @Pattern(message = MESSAGE_PATTERN_ERROR, regexp = "^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$")
     private String fullName;
-    @NotEmpty @Pattern(message = "cpfCnpj does not respect pattern", regexp = "^(\\d{2}\\.?\\d{3}\\.?\\d{3}\\/?\\d{4}-?\\d{2}|\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2})$")
+    @NotEmpty @Pattern(message = MESSAGE_PATTERN_ERROR, regexp = "^(\\d{2}\\.?\\d{3}\\.?\\d{3}\\/?\\d{4}-?\\d{2}|\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2})$")
     private String cpfCnpj;
-    @NotEmpty @Pattern(message = "email does not respect pattern", regexp = "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+    @NotEmpty @Pattern(message = MESSAGE_PATTERN_ERROR, regexp = "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
     private String email;
-    @NotEmpty @Pattern(message = "password does not respect pattern", regexp = "^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$")
+    @NotEmpty @Pattern(message = MESSAGE_PATTERN_ERROR, regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
     private String password;
 }
